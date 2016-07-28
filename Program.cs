3cde114006164f2334595311ae56f109189b6c45
Dario Casula <casula.dario@gmail.com>
2016-07-28 14:23:45 -0500

    removed expiration date

--- Program.cs
+++ Program.cs
@@ -11,20 +11,13 @@ namespace Keyence2IQS
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        static DateTime EXP = new DateTime(2016, 7, 7, 0, 0, 0);
+
         //[STAThread]
         static void Main(string[] args)
         {
-            if (EXP < DateTime.Now)
-            {
-                MessageBox.Show("Sorry, but this application is past its expiration date.  The application will now exit.");
-            }
-            else
-            {
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
-                Application.Run(new dlgMain());
-            }
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new dlgMain());
         }
     }
 }
